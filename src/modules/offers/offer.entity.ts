7835ed7d6dd8b0f5c54998d3d514cea97cf343a3
Roman <chv-roman@mail.ru>
2022-10-14 15:30:28 +0300

    Добавлена валидация

--- src/modules/offers/offer.entity.ts
+++ src/modules/offers/offer.entity.ts
@@ -1,19 +1,18 @@
-// import {Offer} from '../../types/offer.js';
 import typegoose, {getModelForClass, defaultClasses, Ref} from '@typegoose/typegoose';
-// import {
-//   MIN_LENGTH_TITLE,
-//   MAX_LENGTH_TITLE,
-//   MIN_LENGTH_DESCRIPTION,
-//   MAX_LENGTH_DESCRIPTION,
-//   MIN_RATING,
-//   MAX_RATING,
-//   MIN_ROOMS,
-//   MAX_ROOMS,
-//   MIN_ADULTS,
-//   MAX_ADULTS,
-//   MIN_PRICE,
-//   MAX_PRICE
-// } from '../../const.js';
+import {
+  MIN_LENGTH_TITLE,
+  MAX_LENGTH_TITLE,
+  MIN_LENGTH_DESCRIPTION,
+  MAX_LENGTH_DESCRIPTION,
+  MIN_RATING,
+  MAX_RATING,
+  MIN_ROOMS,
+  MAX_ROOMS,
+  MIN_ADULTS,
+  MAX_ADULTS,
+  MIN_PRICE,
+  MAX_PRICE
+} from '../../const.js';
 import { UserEntity } from '../users/user.entity.js';
 
 const {prop, modelOptions} = typegoose;
@@ -26,43 +25,23 @@ export interface OfferEntity extends defaultClasses.Base {}
   }
 })
 export class OfferEntity extends defaultClasses.TimeStamps  {
-  // constructor(data: Offer) {
-  //   super();
 
-  //   this.images = data.images;
-  //   this.title = data.title;
-  //   this.description = data.description;
-  //   this.date =  data.date;
-  //   this.type =  data.type;
-  //   this.bedrooms =  data.bedrooms;
-  //   this.maxAdults = data.maxAdults;
-  //   this.goods = data.goods;
-  //   this.host = data.host;
-  //   this.rating = data.rating;
-  //   this.isFavorite = data.isFavorite;
-  //   this.isPremium = data.isPremium;
-  //   this.price = data.price;
-  //   this.location = data.location;
-  //   this.city = data.city;
-  //   this.previewImage = data.previewImage;
-
-  // }
 
   @prop({required: true, default: []})
   public images!: string[];
 
   @prop({
     required: true,
-    // minlength: [MIN_LENGTH_TITLE, `Min length for title is ${MIN_LENGTH_TITLE}`],
-    // maxlength: [MAX_LENGTH_TITLE, `Max length for title is ${MAX_LENGTH_TITLE}`],
+    minlength: [MIN_LENGTH_TITLE, `Min length for title is ${MIN_LENGTH_TITLE}`],
+    maxlength: [MAX_LENGTH_TITLE, `Max length for title is ${MAX_LENGTH_TITLE}`],
     trim: true,
     default: ''})
   public title!: string;
 
   @prop({required: true,
     trim: true,
-    // minlength: [MIN_LENGTH_DESCRIPTION, `Min length for description is ${MIN_LENGTH_DESCRIPTION}`],
-    // maxlength: [MAX_LENGTH_DESCRIPTION, `Max length for description is ${MAX_LENGTH_DESCRIPTION}`],
+    minlength: [MIN_LENGTH_DESCRIPTION, `Min length for description is ${MIN_LENGTH_DESCRIPTION}`],
+    maxlength: [MAX_LENGTH_DESCRIPTION, `Max length for description is ${MAX_LENGTH_DESCRIPTION}`],
     default: ''})
   public description!: string;
 
@@ -73,14 +52,14 @@ export class OfferEntity extends defaultClasses.TimeStamps  {
   public type!: string;
 
   @prop({required: true,
-    // minlength: [MIN_ROOMS, `Min length for bedrooms is ${MIN_ROOMS}`],
-    // maxlength: [MAX_ROOMS, `Max length for bedrooms is ${MAX_ROOMS}`],
+    minlength: [MIN_ROOMS, `Min length for bedrooms is ${MIN_ROOMS}`],
+    maxlength: [MAX_ROOMS, `Max length for bedrooms is ${MAX_ROOMS}`],
     default: 1})
   public bedrooms!: number;
 
   @prop({required: true,
-    // minlength: [MIN_ADULTS, `Min length for adults is ${MIN_ADULTS}`],
-    // maxlength: [MAX_ADULTS, `Max length for adults is ${MAX_ADULTS}`],
+    minlength: [MIN_ADULTS, `Min length for adults is ${MIN_ADULTS}`],
+    maxlength: [MAX_ADULTS, `Max length for adults is ${MAX_ADULTS}`],
     default: 1})
   public maxAdults!: number;
 
@@ -94,8 +73,8 @@ export class OfferEntity extends defaultClasses.TimeStamps  {
   public host!: Ref<UserEntity>;
 
   @prop({required: true,
-    // minlength: [MIN_RATING, `Min length for rating is ${MIN_RATING}`],
-    // maxlength: [MAX_RATING, `Max length for rating is ${MAX_RATING}`]
+    minlength: [MIN_RATING, `Min length for rating is ${MIN_RATING}`],
+    maxlength: [MAX_RATING, `Max length for rating is ${MAX_RATING}`]
   })
   public rating!: number;
 
@@ -106,8 +85,8 @@ export class OfferEntity extends defaultClasses.TimeStamps  {
   public isPremium!: boolean;
 
   @prop({required: true,
-    // minlength: [MIN_PRICE, `Min length for price is ${MIN_PRICE}`],
-    // maxlength: [MAX_PRICE, `Max length for price is ${MAX_PRICE}`],
+    minlength: [MIN_PRICE, `Min length for price is ${MIN_PRICE}`],
+    maxlength: [MAX_PRICE, `Max length for price is ${MAX_PRICE}`],
     default: 100})
   public price!: number;
 
--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -1,11 +1,11 @@
 import {User} from '../../types/user.js';
 import typegoose, {getModelForClass, defaultClasses} from '@typegoose/typegoose';
 import {createSHA256} from '../../utils/common.js';
-// import {
-//   MIN_LENGTH_NAME,
-//   MAX_LENGTH_NAME,
-//   MIN_LENGTH_PASSWORD,
-//   MAX_LENGTH_PASSWORD} from '../../const.js';
+import {
+  MIN_LENGTH_NAME,
+  MAX_LENGTH_NAME,
+  MIN_LENGTH_PASSWORD,
+  MAX_LENGTH_PASSWORD} from '../../const.js';
 
 const {prop, modelOptions} = typegoose;
 
@@ -36,14 +36,14 @@ export class UserEntity extends defaultClasses.TimeStamps implements User {
   public avatarUrl!: string;
 
   @prop({required: true,
-    // minlength: [MIN_LENGTH_NAME, `Min length for name is ${MIN_LENGTH_NAME}`],
-    // maxlength: [MAX_LENGTH_NAME, `Max length for name is ${MAX_LENGTH_NAME}`],
+    minlength: [MIN_LENGTH_NAME, `Min length for name is ${MIN_LENGTH_NAME}`],
+    maxlength: [MAX_LENGTH_NAME, `Max length for name is ${MAX_LENGTH_NAME}`],
     default: ''})
   public name!: string;
 
   @prop({required: true,
-    // minlength: [MIN_LENGTH_PASSWORD, `Min length for password is ${MIN_LENGTH_PASSWORD}`],
-    // maxlength: [MAX_LENGTH_PASSWORD, `Max length for password is ${MAX_LENGTH_PASSWORD}`],
+    minlength: [MIN_LENGTH_PASSWORD, `Min length for password is ${MIN_LENGTH_PASSWORD}`],
+    maxlength: [MAX_LENGTH_PASSWORD, `Max length for password is ${MAX_LENGTH_PASSWORD}`],
     default: ''})
   private password!: string;
 
